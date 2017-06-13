c3edaf5258950f681882d2f9362264444f7076a5
bunyaminsg <bunyamin.sg@gmail.com>
2017-06-13 15:28:28 +0300

    server address unspecified

--- www/js/services.js
+++ www/js/services.js
@@ -3,28 +3,28 @@ angular.module('starter.services', [])
 .factory('Expenses', function($resource) {
   return {
     all: function() {
-      return $resource('http://146.185.136.134:8080/api/allExpenses',{'query': { method: 'GET' }});
+      return $resource('http://SERVER_ADDRESS/api/allExpenses',{'query': { method: 'GET' }});
     },
     allTime: function() {
-      return $resource('http://146.185.136.134:8080/api/showAllTime',{'query': { method: 'GET' }});
+      return $resource('http://SERVER_ADDRESS:8080/api/showAllTime',{'query': { method: 'GET' }});
     },
     today: function() {
-      return $resource('http://146.185.136.134:8080/api/todayExpenses',{'query': { method: 'GET' }});
+      return $resource('http://SERVER_ADDRESS:8080/api/todayExpenses',{'query': { method: 'GET' }});
     },
     categories: function() {
-      return $resource('http://146.185.136.134:8080/api/monthCategories',{'query': { method: 'GET' }});
+      return $resource('http://SERVER_ADDRESS:8080/api/monthCategories',{'query': { method: 'GET' }});
     },
     add: function() {
-      return $resource('http://146.185.136.134:8080/api/add',{'save':   {method:'POST'}});
+      return $resource('http://SERVER_ADDRESS:8080/api/add',{'save':   {method:'POST'}});
     },
     delete: function(__id) {
-      return $resource('http://146.185.136.134:8080/api/:id',{id: __id},{'delete': {method:'DELETE'}});
+      return $resource('http://SERVER_ADDRESS:8080/api/:id',{id: __id},{'delete': {method:'DELETE'}});
     },
     total: function(_category){
-      return $resource('http://146.185.136.134:8080/api/total/:category',{category: _category},{'query': {method:'GET'}});
+      return $resource('http://SERVER_ADDRESS:8080/api/total/:category',{category: _category},{'query': {method:'GET'}});
     },
     filter: function() {
-      return $resource('http://146.185.136.134:8080/api/filter',{'save':   {method:'POST'}});
+      return $resource('http://SERVER_ADDRESS:8080/api/filter',{'save':   {method:'POST'}});
     },
     newExpense: function(name,category,value) {
       return {
